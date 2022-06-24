b7369709c50a4f8e32f9c0449bc043679bd63388
Pedro Tomaz <pedrohtomaz2021@gmail.com>
2022-06-24 11:09:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# beacademy-devstart-laravel-9
+ Repositório das aulas de Laravel 9 do DevStart (Be Academy e Pay Livre).
---
+++ meu-app-laravel/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{$id}', [UserController::class, 'show'])->name('users.show');
